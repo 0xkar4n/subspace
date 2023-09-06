e49c41c609b19f3c1506051c8caff423daa83bcc
0xkar4n <karandas388@gmail.com>
2023-09-06 19:57:39 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <img src={require('./sub.png')} height={50} width={200} ></img>
         </a>
       
-      <div class="w-24 h-24 absolute rounded-full top-60 left-80y m-2  bg-blue-500"></div>
+      <div class="w-24 h-24 absolute rounded-full top-60 left-80 m-2  bg-blue-500"></div>
       <div class="w-14 h-14 absolute rounded-full top-40 left-60  bg-violet-600"></div>
       <div class="w-14 h-14 absolute rounded-full bottom-10 right-80 m-2  bg-blue-500"></div>
 
